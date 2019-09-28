8dfeec60688246ddff4aa5e7915464c5be3d7ff0
arqdsis <arqdsis@MOOLIi0318.animaedu.intranet>
2019-09-28 15:35:51 -0300

    exercicio 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  contatos = [
+    {
+      id: '1',
+      nome: 'Nathalia',
+      telefone: '119994-7854',
+      email: "nathalia@gmail.com"
+    },
+    {
+      id: '2',
+      nome: 'Lucas',
+      telefone: '119994-7777',
+      email: "lucas@gmail.com"
+    },
+    {
+      id: '3',
+      nome: 'Aldria',
+      telefone: '119994-9999',
+      email: "aldria@gmail.com"
+    }
+  ]
 
+  constructor(public navCtrl: NavController) {
   }
+
+  onItemClick(contato){
+    console.log("item-click", contato);
+  }
+
 
 }
